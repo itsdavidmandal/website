46f44fdfdf0f9b68ee94a3fdc55184b997c68cac
David Ramovich Mandal <davidramovichmandal@gmail.com>
2024-06-04 02:34:24 +0545

    modified the my work section

--- src/Components/My Work/Mywork.jsx
+++ src/Components/My Work/Mywork.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import'./Mywork.css'
-import themepattern from '../../assets/theme_pattern.svg'
-import mywork_data from '../../assets/mywork_data'
-import arrowicon from '../../assets/arrow_icon.svg'
-
+import React from "react";
+import "./Mywork.css";
+import themepattern from "../../assets/theme_pattern.svg";
+import mywork_data from "../../assets/mywork_data";
+import arrowicon from "../../assets/arrow_icon.svg";
 
 const Mywork = () => {
   return (
-    <div id='work' className='mywork'>
-        <div className="mywork-title">
-            <h1>My Latest Work</h1>
-            <img src={themepattern} alt="" />
-        </div>
+    <div id="work" className="mywork">
+      <div className="mywork-title">
+        <h1>My Latest Work</h1>
+        <img src={themepattern} alt="" />
+      </div>
       <div className="mywork-container">
-        {mywork_data.map((work,index)=>{
-            return <img key={index} src={work.w_img} alt="" />
+        {mywork_data.map((work) => {
+          return (
+            <div key={work.w_no} className="mywork-item">
+              <h2>{work.w_title}</h2>
+              <p>{work.w_desc}</p>
+              <div className="mywork-showmore-mini">
+                <p>Show More</p>
+                <img src={arrowicon} alt="" />
+              </div>
+            </div>
+          );
         })}
       </div>
       <div className="mywork-showmore">
@@ -22,7 +30,7 @@ const Mywork = () => {
         <img src={arrowicon} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mywork
+export default Mywork;
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,40 +1,37 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+
 
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_title:"Simple Quiz Website",
+        w_desc :"A simnple website for quiz competitions"
     },
     {
         w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_title:"Web design",
+        w_desc :"A simnple website for quiz competitions"
+
     },
     {
         w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_title:"Web design",
+        w_desc :"A simnple website for quiz competitions"
+
     },
     {
         w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
+        w_title:"Web design",
+        w_desc :"A simnple website for quiz competitions"
     },
     {
         w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
+        w_title:"Web design",
+        w_desc :"A simnple website for quiz competitions"
     },
     {
         w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
+        w_title:"Web design",
+        w_desc :"A simnple website for quiz competitions"
     },
 ]
  
